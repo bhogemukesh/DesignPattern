b57c2be1292f3e30b59ffb51c8b180bcef6a628b
Mukesh bhoge <bhogemukesh@gmail.com>
2021-12-16 22:44:27 +0530

    Changes for adaptor

--- src/main/java/com/mukesh/designpattern/corejava/structural/adaptor/AdaptorMainApp.java
+++ src/main/java/com/mukesh/designpattern/corejava/structural/adaptor/AdaptorMainApp.java
@@ -3,6 +3,8 @@ package com.mukesh.designpattern.corejava.structural.adaptor;
 /**
  * @author Mukesh Bhoge
  * Analogy : Mobile chargers. One charger having adaptors which will help charging different mobile phones
+ * Consider as scenario here one of pear programer only share class file of PilotPen.class and no source code
+ * Now i have to use that in our code to write the assignment then  how to implement that.
  **/
 public class AdaptorMainApp {
 
